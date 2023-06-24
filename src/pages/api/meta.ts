4cf801960c2a78a2dd3d0f2bd5956f8c463790b8
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2023-06-25 07:54:33 +0800

    trim version in api/meta

--- src/pages/api/meta.ts
+++ src/pages/api/meta.ts
@@ -9,6 +9,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const version = fs.readFileSync("version").toString();
+  const version = fs.readFileSync("version").toString().trim();
   res.status(200).json({ version });
 }
